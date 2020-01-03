01eb08870386c6987c03fb8fa5a90a4da13713a8
blomman <johnny@gumbler.com>
2020-01-03 13:04:22 +0100

    Added balls empty component

--- src/components/balls/index.js
+++ src/components/balls/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Balls = styled.div`
-  min-height: 100vh;
-`;
+const Balls = styled.div``;
 
 export default () => {
   return <Balls></Balls>;
