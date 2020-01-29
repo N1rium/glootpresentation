303edb10cc0f5cad0a2acfd52c2a49a82e06480c
blomman <johnny@gumbler.com>
2020-01-29 16:49:14 +0100

    Removed unused code added hooks

--- src/components/market-overview/index.js
+++ src/components/market-overview/index.js
@@ -5,27 +5,16 @@ import {
   InnerContainer,
   Title,
   Description,
-  IconGroup,
-  IconContainer,
-  PlayersContainer,
   PillarContainer,
   PillarInner,
   Pillars,
-  Pillar,
   MusicPillar,
   VideoPillar,
   GreenPillar,
   Indexes,
 } from './style';
-import Reveal from 'react-reveal/Reveal';
 import Slide from 'react-reveal/Slide';
-import Zoom from 'react-reveal/Zoom';
 import Fade from 'react-reveal/Fade';
-
-import MusicIcon from '../../assets/icons/musicicon.svg';
-import GamingIcon from '../../assets/icons/gamesicon.svg';
-import VideoIcon from '../../assets/icons/cameraicon.svg';
-import PlayersIcon from '../../assets/icons/players.svg';
 
 export default ({ scroll: parentScroll }) => {
   const ref = useRef(null);
@@ -57,26 +46,6 @@ export default ({ scroll: parentScroll }) => {
             <br /> the revenue than music & film do combined.
           </Description>
         </Slide>
-        {/* <IconGroup>
-          <Slide left bottom>
-            <IconContainer className="music">
-              <img src={MusicIcon} />
-              <p>$17B</p>
-            </IconContainer>
-          </Slide>
-          <Zoom>
-            <IconContainer className="games">
-              <img src={GamingIcon} />
-              <p>$116B</p>
-            </IconContainer>
-          </Zoom>
-          <Slide right bottom>
-            <IconContainer className="video">
-              <img src={VideoIcon} />
-              <p>$41B</p>
-            </IconContainer>
-          </Slide>
-        </IconGroup> */}
         <PillarContainer>
           <PillarInner>
             <Indexes className="top">
